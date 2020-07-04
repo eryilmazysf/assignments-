554f277f7c404af29e9619d3c65939d81b9daa38
Yusuf Eryilmaz <yusuferyilmaz1819@gmail.com>
2020-07-04 14:55:47 -0400

    first assignment in github

---
+++ assignment9.py
@@ -0,0 +1,7 @@
+name="yusuf"
+password=123456
+user_name=input('enter your name:').lower()
+if (user_name == name):
+    print('welcome {} your password is :{}'.format(user_name,password))
+else:
+    print('hello {} see you later'.format(user_name))
