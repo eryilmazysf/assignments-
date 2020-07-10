610925f499a896297709d096143a2e7bc06155fe
Yusuf Eryilmaz <yusuferyilmaz1819@gmail.com>
2020-07-09 23:54:37 -0400

    prime_number

--- primenumber.py
+++ primenumber.py
@@ -5,17 +5,12 @@ while True:
         print('quit......')
         break
     else:
-        number=int(number)
-        if number==1:
-            print('1 is not a prime number')
-        elif number==2:
-            print('2 is a prime number')
-
+        number = int(number)
         for i in range(2,number):
             if (number%i==0):
                 x=True
-        if (x):
-                print('{} is not a prime number'.format(number))
+        if (x or number==1):
+            print('{} is not a prime number'.format(number))
         else:
-                print('{} is  a prime number'.format(number))
+            print('{} is a prime number'.format(number))
 
